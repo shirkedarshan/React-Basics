07f0aec11ce6c079399b83f2789fc1d22f3d0274
Darshan Shirke <shirkedarshan@gmail.com>
2020-12-30 17:52:11 +0530

    React DOM and JSX Basic Render behaviour

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello world</h1>
+    <p>This is a para</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+var myNewP = document.createElement("p")
+myNewP.innerHTML = "This is a paragraph"
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
