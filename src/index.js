ee4d57311cab7d713ccb899f6df9d704c9b50026
Darshan Shirke <shirkedarshan@gmail.com>
2020-12-30 18:31:45 +0530

    Function save in another folder/file

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './App';
+// import App from './App';
+import MyInfo from './components/MyInfo';
 import reportWebVitals from './reportWebVitals';
 
-function MyInfo(){
-  return (
-    <div>
-      <h1>Darshan  Shirke</h1>
-      <p>This is para about me</p>
-      <ul><b>
-        <li>Raigad</li>
-        <li>Rajgad</li>
-        <li>Jinji</li>
-        </b></ul>
-    </div>
-  )
-}
 ReactDOM.render(
   <React.StrictMode>
     <MyInfo/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-var myNewP = document.createElement("p")
-myNewP.innerHTML = "This is a paragraph"
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
