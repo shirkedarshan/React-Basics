2cf10eaa617d9f3192077563165ae5881b991616
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-12 16:53:36 +0530

    props cat pass data

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import Footer from './Footer';
-import Nav from './Nav';
-import Main from './Main';
+import ContactCard from "./ContactCard"
 
 function App() {
 
-  const date = new Date()
-  const hours = date.getHours()
-
-  const firstName = "Darshan"
-  const lastName = "Shirke"
-
-  const h3Style = {
-    color : "blue",
-    backgroundColor : "Yellow"
-  }
-
-  let timeOfDay
-
-  if ( hours < 12){
-    timeOfDay = "morning"
-    h3Style.color = "green"
-  }else if (hours >= 12 && hours < 16 ){
-    timeOfDay = "afternoon"
-    h3Style.color = "white"
-  }else if (hours >= 16 && hours < 19 ){
-    timeOfDay = "evening"
-    h3Style.color = "blue"
-  }else {
-    timeOfDay = "night"
-    h3Style.color = "red"
-  }
-
   return (
     <div>
-      <h1 className="navbar">Good { timeOfDay } { hours } !</h1>
-      <h2 style={{color:'white',backgroundColor:"#FF2D00"}}> Hello { `${firstName} ${lastName}`}</h2>
-
-      <h3 style={h3Style}> h3 style testing</h3>
+      <ContactCard contact={{name:"Cat Manimau",phone:"986543210",
+      email:"asd@gmail.com"}}/>
+      <ContactCard 
+                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+            
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
 
     </div>
   );
