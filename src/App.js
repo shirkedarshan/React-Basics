c7d283d0ac555f9d9e249adf6b0f7b5128408e0b
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-18 17:30:03 +0530

    State : Evaluation from data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import { render } from "@testing-library/react"
-import React,{Component} from "react"
-import Header from "./Header.js"
+import React from "react"
+
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
 
 class App extends React.Component {
-  constructor(){
-    super()
-    this.state ={
-      answer : "Yes"
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
     }
-  }
-
-  render(){
-    return(
-      <div>
-        <p> My Answer is {answer} </p>
-        <ChildComponent answer={this.state.answer}/>
-      </div>
-    )
-  }
+    
+    render() {
+        let wordDisplay
+        if (this.state.isLoggedIn) {
+            wordDisplay = "in"
+        } else {
+            wordDisplay = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged {wordDisplay}</h1>
+            </div>
+        )
+    }
 }
 
 export default App
