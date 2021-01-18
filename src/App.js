9414c780bd009f6dc242bcd0db55e8acdc7b4529
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-18 16:32:38 +0530

    Class based component : Practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react"
-
+import { render } from "@testing-library/react"
+import React,{Component} from "react"
+import Header from "./Header.js"
 // function App() {
 //   return (
 //     <div>
@@ -12,11 +13,33 @@ class App extends React.Component {
   render(){
     return(
       <div>
-        <p>Code goes here</p> 
-        <h1>{this.props.whatever}</h1>
-    </div>
+        <Header username="vschool"/>
+        <Greeting/>
+      </div>
     )
   }
 }
 
+
+class Greeting extends Component {
+  render(){
+    const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
+        
+        if (hours < 12) {
+            timeOfDay = "morning"
+        } else if (hours >= 12 && hours < 17) {
+            timeOfDay = "afternoon"
+        } else {
+            timeOfDay = "night"
+        }
+    return(
+      <div>
+        <h1>Good {timeOfDay} to you, sir or madam!</h1>
+      </div>
+    )
+  }
+
+}
 export default App
