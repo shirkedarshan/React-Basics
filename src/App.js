ce22b67d82f2de3304c62b5d0cf975d4e7a50309
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-18 17:14:12 +0530

    State syntax & use : Basic

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import { render } from "@testing-library/react"
 import React,{Component} from "react"
 import Header from "./Header.js"
-// function App() {
-//   return (
-//     <div>
-//       <p>Code goes here</p>
-//     </div>
-//   )
-// }
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      answer : "Yes"
+    }
+  }
+
   render(){
     return(
       <div>
-        <Header username="vschool"/>
-        <Greeting/>
+        <p> My Answer is {answer} </p>
+        <ChildComponent answer={this.state.answer}/>
       </div>
     )
   }
 }
 
-
-class Greeting extends Component {
-  render(){
-    const date = new Date()
-        const hours = date.getHours()
-        let timeOfDay
-        
-        if (hours < 12) {
-            timeOfDay = "morning"
-        } else if (hours >= 12 && hours < 17) {
-            timeOfDay = "afternoon"
-        } else {
-            timeOfDay = "night"
-        }
-    return(
-      <div>
-        <h1>Good {timeOfDay} to you, sir or madam!</h1>
-      </div>
-    )
-  }
-
-}
 export default App
