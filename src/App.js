a8958238dce264dd574ee454c5dcec487318f59d
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-20 17:57:54 +0530

    Lifecycle method - componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
+// https://scrimba.com/g/greacthooks
+// Uncaught Invariant Violation: Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent infinite loops.
+
 import React from "react"
+// import randomcolor from "randomcolor"
 
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state={
-      count:0
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0,
+            color: ""
+        }
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
     }
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick(){
-    this.setState( (prevState) => {
-      return{
-        count: prevState.count + 1
-      }
-    })
-  }
-
-  render(){
-    return(
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Click me..!</button>
-      </div>
-    )
-  }
+    
+    increment() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+    decrement() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count - 1
+            }
+        })
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count) {
+            console.log("new")
+            // const newColor = randomcolor()
+            // this.setState({color: newColor})
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1 style={{color: this.state.color}}>{this.state.count}</h1>
+                <button onClick={this.increment}>
+                    Increment!
+                </button>
+                <button onClick={this.decrement}>
+                    Decrement!
+                </button>
+            </div>
+        )
+    }
 }
+
 export default App
