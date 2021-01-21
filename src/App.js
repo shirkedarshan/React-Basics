5cafbe20f9f5c71cc4294e1e63851a9a808cb8cc
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-21 17:37:46 +0530

    Conditional Rendering : LoggedIn/LoggedOut

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React, {Component} from "react"
-import Conditional from "./Conditional"
+import React from "react"
 
-class App extends Component {
+/*
+Challenge:
+
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+    // class-based component
+    // constructor method
+2. Have state keep track of whether the user is logged in or not
+    // isLoggedIn: Boolean (true or false)
+3. Add a button that logs the user in/out
+    // event listener (onClick)
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+        // Conditional Rendering
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+    // Conditional Rendering
+*/
+
+class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            unreadMessages: ["a", "b"]
+            isLoggedIn: false
         }
+        this.handleClick = this.handleClick.bind(this)
     }
-    // &&
-    // false && false
-    render() {
-        return (
+    
+    handleClick() {
+        this.setState( prevState => {
+            return{
+                isLoggedIn : !prevState.isLoggedIn
+            }
+        })
+    }
+
+    render() {   
+        let buttonText = this.state.isLoggedIn ? "Log Out" : "Log In"
+        let displayText = this.state.isLoggedIn ? "LoggedIn" : "LoggedOut"
+
+        return(
             <div>
-                {
-                    this.state.unreadMessages.length > 0 && 
-                    <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
-                }
+                <button onClick={this.handleClick}>{buttonText}</button>
+                <h2>{displayText}</h2>
             </div>
         )
     }
--- src/Conditional.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function Conditional(props) {
-    return <h1>Some cool stuff about conditional rendering</h1>
-}
-
-export default Conditional
