ab0951ee175a28f72edb190d168f8c874ee79f01
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-21 18:29:14 +0530

    Fetching the APIs

--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
-import React from "react"
+import React, {Component} from "react"
 
-/*
-Challenge:
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
 
-Given a stateless functional component:
-1. Follow the steps necessary to add state to it,
-    // class-based component
-    // constructor method
-2. Have state keep track of whether the user is logged in or not
-    // isLoggedIn: Boolean (true or false)
-3. Add a button that logs the user in/out
-    // event listener (onClick)
-    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
-        // Conditional Rendering
-4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
-    // Conditional Rendering
-*/
-
-class App extends React.Component {
+class App extends Component {
     constructor() {
         super()
         this.state = {
-            isLoggedIn: false
+            data : "",
+            loading : false
         }
-        this.handleClick = this.handleClick.bind(this)
     }
     
-    handleClick() {
-        this.setState( prevState => {
-            return{
-                isLoggedIn : !prevState.isLoggedIn
-            }
+    componentDidMount(){
+
+        this.setState( {
+            loading : true
         })
+
+        fetch("https://swapi.dev/api/people/1")
+            .then( response => response.json() )
+            .then( data => { this.setState (
+                 {loading : false,
+                  data : data}
+            )
+            console.log(data)
+        } )
+
     }
-
-    render() {   
-        let buttonText = this.state.isLoggedIn ? "Log Out" : "Log In"
-        let displayText = this.state.isLoggedIn ? "LoggedIn" : "LoggedOut"
-
-        return(
+    
+    render() {
+        const text = this.state.loading ? "Loading..." : this.state.data.name
+        return (
             <div>
-                <button onClick={this.handleClick}>{buttonText}</button>
-                <h2>{displayText}</h2>
+               <p>{text}</p>
             </div>
         )
     }
