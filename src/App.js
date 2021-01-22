db06776c9ee69386904e965cb332c1059cc9bc69
Darshan Shirke <shirkedarshan@gmail.com>
2021-01-22 18:24:47 +0530

    Hooks : useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react"
-import Header from "./Header"
-import MemeGenerator from "./MemeGenerator"
-import "./App.css"
+import React, {useState} from "react"
 
 function App() {
+
+    const value = useState("200")
+    const data = {
+        name:"abc",
+        age:"11"
+    }
+    const {name,age} = data
+
+    const [count , setCount] = useState(0)
+
+    function increment(){
+        setCount(Hot => Hot + 1)
+    }
+
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    console.log(data,count)
+
     return (
         <div>
-            <Header />
-            <MemeGenerator />
+            <h2>Part 1</h2>
+            {value}
+            <h2>Part 2</h2>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
